a7ab2f06421cec8fa722497560d3d71932a30e80
Farhan Mahmood <farhanmahmoodny@gmail.com>
2020-11-26 01:15:43 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -2,7 +2,12 @@ def first_challenge
    epic_tragedy = {
      :montague => {
        :patriarch => {
-         
+         :name => "Lord Montague",
+         :age => "53"
+       },
+       :matriarch => {
+         :name => "Lady Montague",
+         :age => "54"
        }
      }
    }
